37ef503eed1bef2ce04cd8b5a3355142d9904931
svdcvt <sadymchenko@edu.hse.ru>
2020-03-14 19:10:44 +0300

    small fixes

--- src/food/datasets/cifar.py
+++ src/food/datasets/cifar.py
@@ -1,16 +1,16 @@
-from torchvision.datasets import CIFAR10, CIFAR100
 import numpy as np
+from torchvision.datasets import CIFAR10, CIFAR100
 
 
 class CIFAR_10(CIFAR10):
-    def __init__(self, datapath : str, task="vanilla", mode="train"):
-        super().__init__(datapath, train=(mode=="train"), download=True)
+    def __init__(self, datapath: str, task="vanilla", mode="train"):
+        super().__init__(datapath, train=(mode == "train"), download=True)
         if task == "ood":
             self.targets = np.array(self.targets)
-            ood_idx = (self.targets >= 5)
+            not_ood = (self.targets < 5)
             if mode == "train":
-                self.data = self.data[ood_idx]
-                self.targets = self.targets[ood_idx]
+                self.data = self.data[not_ood]
+                self.targets = self.targets[not_ood]
             elif mode == "val":
                 self.targets[self.targets > 5] = 5
             else:
@@ -23,14 +23,14 @@ class CIFAR_10(CIFAR10):
 
 
 class CIFAR_100(CIFAR100):
-    def __init__(self, datapath:str, task="vanilla", mode="train"):
-        super().__init__(datapath, train=(mode=="train"), download=True)
+    def __init__(self, datapath: str, task="vanilla", mode="train"):
+        super().__init__(datapath, train=(mode == "train"), download=True)
         if task == "ood":
             self.targets = np.array(self.targets)
-            ood_idx = (self.targets >= 50)
+            not_ood = (self.targets < 50)
             if mode == "train":
-                self.data = self.data[ood_idx]
-                self.targets = self.targets[ood_idx]
+                self.data = self.data[not_ood]
+                self.targets = self.targets[not_ood]
             elif mode == "val":
                 self.targets[self.targets > 50] = 50
             else:
