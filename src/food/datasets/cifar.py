13a47fd7186c0ad2b5da4d8345a2d600605548ff
svdcvt <sadymchenko@edu.hse.ru>
2020-03-15 16:57:42 +0300

    adding mnist dataset, optimizing tinyimagenet reading and attribute fixes

--- src/food/datasets/cifar.py
+++ src/food/datasets/cifar.py
@@ -1,10 +1,22 @@
 import numpy as np
 from torchvision.datasets import CIFAR10, CIFAR100
+from albumentations.pytorch import ToTensorV2 as ToTensor
 
 
 class CIFAR_10(CIFAR10):
-    def __init__(self, datapath: str, task="vanilla", mode="train"):
+    '''
+    Attributes:
+        class_to_id: dict, bijection from name of class to number of class 0 ... K
+        tag_to_class: dict, map from number of class 0 ... K to new number of class 0 ... K / 2 (K if vanilla task)
+    '''
+    def __init__(self, datapath: str, task="vanilla", mode="train", transform=ToTensor()):
+        if task not in ["vanilla", "ood"]:
+            raise RuntimeError(f"Unknown task {task}")  
         super().__init__(datapath, train=(mode == "train"), download=True)
+        self.transform = lambda x: transform(force_apply=True, image=x)["image"]
+        self.tag_to_class = self.class_to_idx.copy()
+        self.class_to_id = {cl : cl for cl in self.tag_to_class.values()}
+        
         if task == "ood":
             self.targets = np.array(self.targets)
             not_ood = (self.targets < 5)
@@ -15,16 +27,23 @@ class CIFAR_10(CIFAR10):
                 self.targets[self.targets > 5] = 5
             else:
                 raise RuntimeError(f"Unknown mode {mode}")
-            self.class_to_idx = {cl : min(idx, 5) for cl, idx in self.class_to_idx.items()}
-        elif task == "vanilla":
-            pass
-        else:
-            raise RuntimeError(f"Unknown task {task}")
+            self.class_to_id = {idx : min(idx, 5) for cl, idx in self.class_to_idx.items()}
 
 
 class CIFAR_100(CIFAR100):
-    def __init__(self, datapath: str, task="vanilla", mode="train"):
+    '''
+    Attributes:
+        class_to_id: dict, bijection from name of class to number of class 0 ... K
+        tag_to_class: dict, map from number of class 0 ... K to new number of class 0 ... K / 2 (K if vanilla task)
+    '''
+    def __init__(self, datapath: str, task="vanilla", mode="train", transform=ToTensor()):
+        if task not in ["vanilla", "ood"]:
+            raise RuntimeError(f"Unknown task {task}")
         super().__init__(datapath, train=(mode == "train"), download=True)
+        self.transform = lambda x: transform(force_apply=True, image=x)["image"]
+        self.tag_to_class = self.class_to_idx.copy()
+        self.class_to_id = {cl : cl for cl in self.tag_to_class.values()}
+        
         if task == "ood":
             self.targets = np.array(self.targets)
             not_ood = (self.targets < 50)
@@ -35,8 +54,4 @@ class CIFAR_100(CIFAR100):
                 self.targets[self.targets > 50] = 50
             else:
                 raise RuntimeError(f"Unknown mode {mode}")
-            self.class_to_idx = {cl : min(idx, 50) for cl, idx in self.class_to_idx.items()}
-        elif task == "vanilla":
-            pass
-        else:
-            raise RuntimeError(f"Unknown task {task}")
+            self.class_to_id = {idx : min(idx, 50) for cl, idx in self.class_to_idx.items()}
---
+++ src/food/datasets/mnist.py
@@ -0,0 +1,36 @@
+import numpy as np
+from torchvision.datasets import MNIST
+from albumentations.pytorch import ToTensorV2 as ToTensor
+
+
+class MNIST2(MNIST):
+    '''
+    Attributes:
+        class_to_id: dict, bijection from name of class to number of class 0 ... 9
+        tag_to_class: dict, map from number of class 0 1 2 3 4 5 6 7 8 9
+                            to new number of class 5 5 0 1 5 2 3 5 4 5 
+                            (0 1 2 3 4 5 6 7 8 9 if vanilla task)
+    '''
+    def __init__(self, datapath: str, task="vanilla", mode="train", transform=ToTensor()):
+        if task not in ["vanilla", "ood"]:
+            raise RuntimeError(f"Unknown task {task}")
+        if mode not in ["train", "val"]:
+            raise RuntimeError(f"Unknown mode {mode}")       
+        super().__init__(datapath, train=(mode == "train"), download=True)
+        
+        self.transform = lambda x: transform(force_apply=True, image=x)["image"]
+        self.tag_to_class = self.class_to_idx.copy()
+        self.class_to_id = {cl : cl for cl in self.tag_to_class.values()}
+        
+        if task == "ood":
+            not_ood_digits = [2, 3, 5, 6, 8]
+            ood_digits = [0, 1, 4, 7, 9]
+            self.class_to_id = {d : min(i, 5) for i, d in enumerate(not_ood_digits + ood_digits)}
+            self.target_transform = lambda x: self.class_to_id[x]
+            
+            self.not_ood_digits = set(not_ood_digits)
+            targets_numpy = self.targets.detach().numpy()
+            not_ood_idx = np.where([x in self.not_ood_digits for x in targets_numpy])
+            if mode == "train":
+                self.data = self.data[not_ood_idx]
+                self.targets = self.targets[not_ood_idx]
