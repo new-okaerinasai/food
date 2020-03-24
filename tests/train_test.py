29b16bb165621e6a8d7ea027d80e74d949f59316
enotdima <20352302+enotdima@users.noreply.github.com>
2020-03-25 00:24:42 +0300

    Create train_test.py
    test for train_loop

---
+++ tests/train_test.py
@@ -0,0 +1,18 @@
+import food
+from food.datasets import TinyImagenet, MNIST, FashionMNIST, CIFAR_10, CIFAR_100
+from food.experiments.train_vanilla import evaluate, train
+import torch
+
+def test_train(models):
+    datasets = ['tiny_imagenet', 'cifar_100', 'cifar_10']
+    for model in models
+        for dataset in datasets:
+            batch_size=512
+            logits, loss, predictions, val_loss, val_acc = train(model=model, dataset=dataset, epochs=1, test=True, batch_size=batch_size)
+            
+            assert logits.shape[0] == batch_size
+            assert len(predictions) == batch_size
+            assert loss > 0
+            assert val_loss > 0
+            assert val_acc > 0
+
