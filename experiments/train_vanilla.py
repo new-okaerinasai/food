50ded4541687ebe13f382c561983dade5bc751cf
enotdima <20352302+enotdima@users.noreply.github.com>
2020-03-31 20:40:04 +0300

    Update train_vanilla.py

--- experiments/train_vanilla.py
+++ experiments/train_vanilla.py
@@ -53,128 +53,75 @@ def evaluate(model, dataloader, criterion, device, train_writer) -> Tuple:
         print("  Evaluation results: \n   Accuracy: {:.4f}\n   Loss: {:.4f}".format(accuracy, loss))
     return loss, accuracy
 
-
 def train(**kwargs):
-    
-    
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_path", default="./data", type=str,
-                        help="path where data is kept or will be downloaded")
-    parser.add_argument("--batch_size", default=512, type=int,
-                        help="um... batch size during training and testing, I guess?")
-    parser.add_argument('--model', default="resnet18", type=str,
-                        help="model to train. Must be resnet18 or resnet50")
-    parser.add_argument('--dataset', default="tiny_imagenet", type=str,
-                        help="dataset on which we train. Must be \'tiny_imagenet\' or 'cifar_100")
-    parser.add_argument("--lr", default=1e-3, type=float,
-                        help="learning rate. Must be float")
-    parser.add_argument('--epochs', default=10, type=int,
-                        help="number of training epochs")
-    parser.add_argument('--logdir', default="./logs",
-                        help="directory where logs for tensorboard will be stored. Training plots can be\
-                         viewed with ```tensroboard --logdir=logdir```")
-    parser.add_argument('--checkpoints_dir', default="./checkpoints",
-                        help="directory where checkpoints will be stored")
-    parser.add_argument('--checkpoint_each', default=20, type=int,
-                        help="number of steps -- checkpointing interval")
-    parser.add_argument('--log_each', default=10, type=int,
-                        help="number of steps -- logging interval for tensorboard")
-    parser.add_argument('--keep_logs', action="store_true",
-                        help="set this to keep old logs in logdir if it exists")
-    parser.add_argument('--resume', type=str, default=None,
-                        help="path to a previous checkpointt to continue training")
-    parser.add_argument('--task', type=str, default='ood', choices=['vanilla', 'ood'],
-                        help="task that we solve. Choose 'ood' to train the ood detector.")
+    parser.add_argument("--config", default="./vanilla.json", type=str,
+                        help="path to the config file")
     args = parser.parse_args()
-    if args.task.lower() == 'ood':
-        n_classes = {"tiny_imagenet": 100, "cifar_100": 50}
-    else:
-        n_classes = {"tiny_imagenet": 200, "cifar_100": 100}
-    get_with_arg = {"tiny_imagenet": food.datasets.TinyImagenet, "cifar_100": food.datasets.CIFAR_100,
-                    "cifar_10": food.datasets.CIFAR_10}
+    args = Config(args.config)
+    get_dataset_with_arg = {"tiny_imagenet": food.datasets.TinyImagenet,
+                            "cifar_100": food.datasets.CIFAR_100,
+                            "cifar_10": food.datasets.CIFAR_10}
     if not args.keep_logs:
         try:
             shutil.rmtree(args.logdir)
         except FileNotFoundError:
             pass
-    
+
     batch_size=args.batch_size
     batch_size = kwargs.get('batch_size', batch_size)
-    
+
     model=args.model.lower()
     model = kwargs.get('model', model).lower()
-    
+
     dataset = args.dataset.lower()
     dataset = kwargs.get('dataset', dataset).lower()
-    
+
     epochs = args.epochs
     epochs = kwargs.get('epochs', epochs)
-    
+
     test_b = kwargs.get('test', False)
 
-    
-    device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    if model.lower() == "resnet18":
-        train_transforms = Compose([
-            RandomBrightnessContrast(p=0.5),
-            Rotate(20),
-            HorizontalFlip(0.5),
-            Normalize([0.4802, 0.4481, 0.3975], [0.2302, 0.2265, 0.2262]),
-            ToTensor(),
-        ], p=1)
-        val_transforms = Compose([
-            Normalize([0.4802, 0.4481, 0.3975], [0.2302, 0.2265, 0.2262]),
-            ToTensor(),
-        ], p=1)
-        model = resnet18(num_classes=n_classes[dataset.lower()])
-    elif model.lower() == "resnet50":
-        train_transforms = Compose([
-            ToTensor()
-        ])
-        val_transforms = Compose([
-            ToTensor()
-        ])
-        model = resnet50(num_classes=n_classes[dataset])
-    else:
-        raise NotImplementedError("Unknown model".format(model))
-    model.to(device)
+    ds_class = get_dataset_with_arg[dataset.lower()]
 
-    ds_class = get_with_arg[dataset.lower()]
-    if dataset.lower() == 'tiny_imagenet':
-        train_dataset = food.datasets.TinyImagenet(args.data_path, mode="train", task=args.task.lower(),
-                                                   transform=train_transforms)
-        val_dataset = food.datasets.TinyImagenet(args.data_path, mode="val", task=args.task.lower(),
-                                                 transform=val_transforms)
-        ood_label = 100
-    elif dataset.lower() == "cifar_100":
-        train_dataset = food.datasets.CIFAR_100(args.data_path, mode="train", transform=train_transforms,
-                                                task=args.task.lower())
-        val_dataset = food.datasets.CIFAR_100(args.data_path, mode="val", transform=val_transforms,
-                                              task=args.task.lower())
-        ood_label = 50
-    elif dataset.lower() == "cifar_10":
-        train_dataset = food.datasets.CIFAR_10(args.data_path, mode="train", task=args.task.lower(),
-                                               transform=train_transforms)
-        val_dataset = food.datasets.CIFAR_10(args.data_path, mode="val", task=args.task.lower(),
-                                             transform=val_transforms)
-        ood_label = 5
-    else:
-        raise NotImplementedError("Unknown dataset {}".format(dataset))
-    
-    
+    train_transforms = Compose([
+            HorizontalFlip(p=0.5),
+            RandomResizedCrop(32, 32, p=0.5),
+            Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            ToTensor(),
+    ], p=1)
+    val_transforms = Compose([
+        Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ToTensor(),
+    ], p=1)
+
+    train_dataset = ds_class(args.data_path, mode="train", task=args.task.lower(),
+                                                transform=train_transforms)
+    val_dataset = ds_class(args.data_path, mode="val", task=args.task.lower(),
+                                                transform=val_transforms)
+    print(train_dataset[0])
+    model = getattr(torchvision.models, args.model)(num_classes=train_dataset.n_classes)
+    print(model)
+    print(model.__class__.__name__)
+    print("Total number of model's parameters: ",
+          np.sum([p.numel() for p in model.parameters() if p.requires_grad]))
     #batch_size=args.batch_size
     #batch_size = kwargs.get('batch_size', batch_size)
-    
-    
+
+
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
     val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, drop_last=False)
     # if torch.cuda.is_available(): TODO
     #    train_dataloader = DataPrefetcher(train_dataloader)
     #    val_dataloader = DataPrefetcher(val_dataloader)
     #    pass
+
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    model.to(device)
+
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(lr=args.lr, params=model.parameters())
-    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.9)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [50, 100, 200], gamma=0.1) #ExponentialLR(optimizer, 0.9)
 
     if args.resume is not None:
         state_dict = torch.load(args.resume, map_location=device)
@@ -182,22 +129,26 @@ def train(**kwargs):
         optimizer.load_state_dict(state_dict["optimizer_state_dict"])
 
     train_writer = SummaryWriter(os.path.join(args.logdir, "train_logs"))
+    val_writer = SummaryWriter(os.path.join(args.logdir, "val_logs"))
     global_step = 0
     for epoch in range(epochs):
         print(f"Training, epoch {epoch + 1}")
         model.train()
-        val_writer = SummaryWriter(os.path.join(args.logdir, f"val_logs_{epoch}"))
         for images, labels in train_dataloader:
             images, labels = images.to(device), labels.to(device)
             optimizer.zero_grad()
+            if global_step == 0:
+                train_writer.add_graph(model, images)
             logits = model.forward(images)
             loss = criterion(logits, labels)
             predictions = logits.argmax(dim=1)
             accuracy_t = torch.mean((predictions == labels).float()).item()
             if global_step % args.log_each == 0:
+                # TODO
                 # train_writer.add_scalar("Loss_BCE", loss, global_step=global_step)
                 # train_writer.add_scalar("Accuracy", accuracy_t, global_step=global_step)
                 # train_writer.add_scalar("Learning_rate", scheduler.get_lr()[-1], global_step=global_step)
+                #log_dict_with_writer(labels, logits, train_writer, global_step=global_step)
                 pass
             loss.backward()
             optimizer.step()
@@ -206,15 +157,17 @@ def train(**kwargs):
         val_loss, val_acc = evaluate(model, val_dataloader, criterion, device, val_writer)
         val_writer.add_scalar("Loss_BCE", val_loss, global_step=global_step)
         val_writer.add_scalar("Accuracy", val_acc, global_step=global_step)
-        os.makedirs(args.checkpoints_dir, exist_ok=True)
-        print("Saving checkpoint...")
-        with open(os.path.join(args.checkpoints_dir, f"epoch_{epoch}_{global_step}.pt"), "wb") as f:
-            torch.save({"model_state_dict": model.state_dict(),
-                        "optimizer_state_dict": optimizer.state_dict()}, f)
+
+        if epoch % args.checkpoint_each == 0:
+            print("Saving checkpoint...")
+            with open(os.path.join(args.checkpoints_dir, f"epoch_{epoch}_{global_step}.pt"), "wb") as f:
+                torch.save({"model_state_dict": model.state_dict(),
+                            "optimizer_state_dict": optimizer.state_dict()}, f)
         scheduler.step()
-    
+
     if test_b:
         return logits, loss, predictions, val_loss, val_acc
+
 
 if __name__ == '__main__':
     train()
