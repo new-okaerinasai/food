09a085c39f9c654649065fce83d4848e84405e33
Angelina-Yaroshenko <43121111+Angelina-Yaroshenko@users.noreply.github.com>
2020-04-03 11:40:07 +0300

    Add files via upload

--- src/food/ood/__init__.py
+++ src/food/ood/__init__.py
@@ -1,3 +1,4 @@
 from .angle_softmax import AngleSoftmax, AngleLinear
+from .loss import glass_loss
 
-__all__ = ["AngleLinear", "AngleSoftmax"]
+__all__ = ["AngleLinear", "AngleSoftmax","glass_loss"]
