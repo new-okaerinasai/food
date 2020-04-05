fa4d762ac6166e7c75a1e1d6672480f8b3c162aa
enotdima <20352302+enotdima@users.noreply.github.com>
2020-04-05 16:19:38 +0300

    Update cifar.py

--- src/food/datasets/cifar.py
+++ src/food/datasets/cifar.py
@@ -9,27 +9,53 @@ class CIFAR_10(CIFAR10):
         class_to_id: dict, bijection from name of class to number of class 0 ... K
         tag_to_class: dict, map from number of class 0 ... K to new number of class 0 ... K / 2 (K if vanilla task)
     '''
-    def __init__(self, datapath: str, task="vanilla", mode="train", transform=ToTensor()):
+    def __init__(self, datapath: str, task="vanilla", mode="train", ood_mode=True, transform=ToTensor()):
         if task not in ["vanilla", "ood"]:
             raise RuntimeError(f"Unknown task {task}")
         super().__init__(datapath, train=(mode == "train"), download=True)
         self.transform = lambda x: transform(force_apply=True,
                                              image=np.array(x))["image"]
+        
+
         self.tag_to_class = self.class_to_idx.copy()
         self.class_to_id = {cl : cl for cl in self.tag_to_class.values()}
         self.n_classes = 10
+        
+        
         if task == "ood":
-            self.n_classes = 5
-            self.targets = np.array(self.targets)
-            not_ood = (self.targets < 5)
-            if mode == "train":
-                self.data = self.data[not_ood]
-                self.targets = self.targets[not_ood]
-            elif mode == "val":
-                self.targets[self.targets > 5] = 5
+            
+            if ood_mode == True:
+                dataset = SVHN(datapath, download=True, transform=ToTensor())
+                self.n_classes = 10
+                self.targets = np.array(self.targets)
+    
+                if mode == "train":
+                    self.data = self.data
+                    self.targets = self.targets
+                elif mode == "val":
+                    self.data = np.vstack((self.data, np.reshape(dataset.data[:50000], (50000, 32, 32, 3))))#self.data + dataset.data
+                    self.targets = np.append(np.array(self.targets), np.full(dataset.data.shape[0], 10))
+    
+                else:
+                    raise RuntimeError(f"Unknown mode {mode}")
+                self.class_to_idx['ood'] = 10
+                self.tag_to_class = self.class_to_idx.copy()
+                self.class_to_id = {cl : cl for cl in self.tag_to_class.values()}
+                self.class_to_id = {idx : idx for cl, idx in self.class_to_idx.items()}
+            
             else:
-                raise RuntimeError(f"Unknown mode {mode}")
-            self.class_to_id = {idx : min(idx, 5) for cl, idx in self.class_to_idx.items()}
+                self.n_classes = 5
+                self.targets = np.array(self.targets)
+                not_ood = (self.targets < 5)
+                if mode == "train":
+                    self.data = self.data[not_ood]
+                    self.targets = self.targets[not_ood]
+                elif mode == "val":
+                    self.targets[self.targets > 5] = 5
+                else:
+                    raise RuntimeError(f"Unknown mode {mode}")
+                self.class_to_id = {idx : min(idx, 5) for cl, idx in self.class_to_idx.items()}
+                
 
 
 class CIFAR_100(CIFAR100):
